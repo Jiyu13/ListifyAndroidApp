d0e096c2f62f2cd00a6629f668d3ca775af68e0b
jiyu13 <jiyucoding@yahoo.com>
2025-07-18 16:27:16 -0400

    - create Lists screen composable

---
+++ app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.listifyjetapp.screens.lists
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ListifyListsScreen(navController: NavHostController) {
+    Text(text = "Lists Screen")
+}
