d4b1d87016b5608bd479183574f8cf9b4e082d10
jiyu13 <jiyucoding@yahoo.com>
2025-07-18 14:21:18 -0400

    - define app colours

--- app/src/main/java/com/example/listifyjetapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object ListifyColor {
+    val SplashYellow = Color(0xFFFFCA3A)
+    val TextDark = Color(0xff3e4e50)
+    val TextGrey = Color(0xff858585)
+}
