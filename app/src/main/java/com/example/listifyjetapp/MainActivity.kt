72886e07c1e56f127c3253bc2cdc639562ca59c1
jiyu13 <jiyucoding@yahoo.com>
2025-07-17 21:24:16 -0400

    - restructure composable - setup ListifyApp() as root Composable - set up navigation in ListifyApp()

--- app/src/main/java/com/example/listifyjetapp/MainActivity.kt
+++ app/src/main/java/com/example/listifyjetapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.listifyjetapp.navigation.ListifyNavigation
 import com.example.listifyjetapp.ui.theme.ListifyJetAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,30 +25,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListifyJetAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            ListifyApp()
+        }
+    }
+}
+
+@Composable
+fun ListifyApp() {
+    ListifyJetAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Surface(
+                modifier = Modifier.padding(innerPadding).fillMaxSize()
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ListifyNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ListifyJetAppTheme {
-        Greeting("Android")
+        ListifyNavigation()
     }
 }
