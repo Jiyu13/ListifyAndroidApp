20163ed4e81b2a9b9cca6665b56b73c942423825
jiyu13 <jiyucoding@yahoo.com>
2025-07-21 21:10:30 -0400

    - create ListViewModel - create observable list state - call repository to fetch user lists

---
+++ app/src/main/java/com/example/listifyjetapp/screens/lists/ListsViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.listifyjetapp.screens.lists
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.listifyjetapp.data.ListifyResult
+import com.example.listifyjetapp.model.ListModel
+import com.example.listifyjetapp.repository.ListsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ListsViewModel @Inject constructor(private val repository: ListsRepository): ViewModel() {
+    val lists = mutableStateListOf<ListModel>()
+
+    fun getUserLists(userId: Int) {
+        viewModelScope.launch {
+            val result = repository.getUserLists(userId = userId)
+            when(result) {
+                is ListifyResult.Success -> {
+                    lists.clear()
+                    lists.addAll(result.data)
+                }
+                is ListifyResult.Failure -> Unit
+            }
+        }
+    }
+}
