c190a44180295f122cd6f84653f1e92969eb29bc
jiyu13 <jiyucoding@yahoo.com>
2025-07-21 21:46:52 -0400

    - displays user lists retrieved from the ViewModel

--- app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
@@ -1,10 +1,40 @@
 package com.example.listifyjetapp.screens.lists
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun ListifyListsScreen(navController: NavHostController) {
-    Text(text = "Lists Screen")
+fun ListifyListsScreen(
+    viewModel: ListsViewModel = hiltViewModel()
+) {
+    LaunchedEffect(Unit) { viewModel.getUserLists(4) }
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        Surface(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        ) {
+
+            if (viewModel.lists.isEmpty()) {
+                Text(text = "You don't have any lists yet.")
+            } else {
+                LazyColumn {
+                    items(viewModel.lists) {list ->
+                        Text(text = list.name)
+                    }
+                }
+            }
+        }
+    }
+
 }
