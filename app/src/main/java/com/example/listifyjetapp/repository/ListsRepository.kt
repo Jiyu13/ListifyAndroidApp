162dc96af3348b8fadd8651127deac939b30f315
jiyu13 <jiyucoding@yahoo.com>
2025-07-21 20:01:56 -0400

    - create ListsRepository data source class - call api to fetch user lists - wrap result inside ListifyResult to handle success / failure

---
+++ app/src/main/java/com/example/listifyjetapp/repository/ListsRepository.kt
@@ -0,0 +1,21 @@
+package com.example.listifyjetapp.repository
+
+import android.util.Log
+import com.example.listifyjetapp.data.ListifyResult
+import com.example.listifyjetapp.model.ListModel
+import com.example.listifyjetapp.network.ListifyAPI
+import javax.inject.Inject
+
+
+class ListsRepository@Inject constructor(private val api: ListifyAPI){
+
+    suspend fun getUserLists(userId: Int): ListifyResult<List<ListModel>> {
+        try {
+            val response = api.getListsByUser(userId)
+            //Log.d("userLists", "userLists: $response")
+            return ListifyResult.Success(data = response)
+        } catch (e:Exception) {
+            return ListifyResult.Failure(e.message ?: "Error fetching lists")
+        }
+    }
+}
