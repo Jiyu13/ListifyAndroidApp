8c72c76de01e7b6a69f5e3c5009824a054cb03a6
chickendude <pokemaster103@gmail.com>
2025-07-25 12:45:51 -0400

    Remove unused import

--- app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.listifyjetapp.screens.lists
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
