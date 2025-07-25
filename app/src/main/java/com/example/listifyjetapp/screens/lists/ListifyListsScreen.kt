d215f61d6b232ccf8659beaab0f470bb1b1bab22
chickendude <pokemaster103@gmail.com>
2025-07-25 12:41:17 -0400

    Extract string resource + formatting

--- app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/screens/lists/ListifyListsScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.listifyjetapp.R
 
 @Composable
 fun ListifyListsScreen(
@@ -20,16 +22,16 @@ fun ListifyListsScreen(
     LaunchedEffect(Unit) { viewModel.getUserLists(4) }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
         Surface(
-            modifier = Modifier.fillMaxSize().padding(innerPadding)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-
             if (viewModel.lists.isEmpty()) {
-                Text(text = "You don't have any lists yet.")
+                Text(text = stringResource(R.string.no_lists))
             } else {
                 LazyColumn {
-                    items(viewModel.lists) {list ->
+                    items(viewModel.lists) { list ->
                         Text(text = list.name)
                     }
                 }
