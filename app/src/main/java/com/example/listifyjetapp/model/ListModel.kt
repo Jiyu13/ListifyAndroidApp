192e5f44b67e564810b5eb86a9cd9a544310a487
jiyu13 <jiyucoding@yahoo.com>
2025-07-30 13:37:51 -0400

    - add BaseList + listName models

--- app/src/main/java/com/example/listifyjetapp/model/ListModel.kt
+++ app/src/main/java/com/example/listifyjetapp/model/ListModel.kt
@@ -2,12 +2,24 @@ package com.example.listifyjetapp.model
 
 import com.google.gson.annotations.SerializedName
 
-data class ListModel(
+data class BaseList(
     val id: Int,
     val name: String,
     val share: Boolean,
     //val sharedCode: String,  // can be UUID or String
     //val createdAt: LocalDateTime
+    @SerializedName("shared_code") val sharedCode: String,
+    @SerializedName("created_at") val createdAt: String,
+)
+
+data class ListName (
+    val name: String
+)
+
+data class ListModel(
+    val id: Int,
+    val name: String,
+    val share: Boolean,
     @SerializedName("shared_code") val sharedCode: String,
     @SerializedName("created_at") val createdAt: String,
     @SerializedName("item_count") val itemCount: Int,
