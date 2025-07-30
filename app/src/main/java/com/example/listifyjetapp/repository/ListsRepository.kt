dadbbe835c7c3da1f3219a08cc4edbe1a49cd04d
jiyu13 <jiyucoding@yahoo.com>
2025-07-30 17:11:14 -0400

    - create insertListByUser api call - call insertList with userId + new list data - return a formatted ListModel response

--- app/src/main/java/com/example/listifyjetapp/repository/ListsRepository.kt
+++ app/src/main/java/com/example/listifyjetapp/repository/ListsRepository.kt
@@ -1,8 +1,8 @@
 package com.example.listifyjetapp.repository
 
-import android.util.Log
 import com.example.listifyjetapp.data.ListifyResult
 import com.example.listifyjetapp.model.ListModel
+import com.example.listifyjetapp.model.ListName
 import com.example.listifyjetapp.network.ListifyAPI
 import javax.inject.Inject
 
@@ -16,6 +16,24 @@ class ListsRepository@Inject constructor(private val api: ListifyAPI){
             return ListifyResult.Success(data = response)
         } catch (e:Exception) {
             return ListifyResult.Failure(e.message ?: "Error fetching lists")
+        }
+    }
+
+    suspend fun insertListByUser(userId: Int, newListData: ListName): ListifyResult<ListModel> {
+        try {
+            val response = api.insertList(userId, newListData)
+            val formatedResponse = ListModel (
+                id = response.id,
+                name = response.name,
+                share = response.share,
+                sharedCode = response.sharedCode,
+                createdAt = response.createdAt,
+                itemCount = 0,
+                sharedWith= emptyList()
+            )
+            return ListifyResult.Success(data = formatedResponse)
+        } catch (e:Exception) {
+            return ListifyResult.Failure(e.message ?: "Error creating new list")
         }
     }
 }
