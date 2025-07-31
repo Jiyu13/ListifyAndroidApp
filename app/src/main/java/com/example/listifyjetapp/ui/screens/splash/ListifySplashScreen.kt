396e8241ab3298ac720526b66ac4065bf1fcf295
jiyu13 <jiyucoding@yahoo.com>
2025-07-31 12:16:47 -0400

    - move button columns to a separate file

--- app/src/main/java/com/example/listifyjetapp/ui/screens/splash/ListifySplashScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/screens/splash/ListifySplashScreen.kt
@@ -3,25 +3,9 @@ package com.example.listifyjetapp.ui.screens.splash
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,14 +15,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.listifyjetapp.R
-import com.example.listifyjetapp.ui.navigation.ListifyScreens
+import com.example.listifyjetapp.components.authButtons.authButtons
 import com.example.listifyjetapp.ui.theme.ListifyColor
 import com.example.listifyjetapp.ui.theme.barriecitoFont
 import kotlinx.coroutines.delay
@@ -91,56 +73,7 @@ fun ListifySplashScreen(navController: NavHostController) {
         }
 
         if (isShowButtons.value) {
-            Column(
-                modifier = Modifier.fillMaxSize().padding(16.dp),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                OutlinedButton(
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    border = BorderStroke(2.dp, color = Color.White),
-
-                    onClick = {  }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.MailOutline,
-                        modifier = Modifier.size(20.dp),
-                        contentDescription = "Sign In Email",
-                        )
-                    Text(
-                        text = "SIGN UP WITH EMAIL",
-                        modifier = Modifier.padding(8.dp),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp,
-
-                    )
-                }
-
-                Button(
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
-                    shape = RoundedCornerShape(10.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.White,
-                        contentColor = ListifyColor.TextDark
-                    ),
-                    onClick = {  }
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ExitToApp,
-                        modifier = Modifier.size(20.dp),
-                        contentDescription = "Log In"
-                    )
-
-                    Text(
-                        text = "LOG IN ",
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = 20.sp,
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
-            }
-
+            authButtons(navController)
         }
     }
 }
