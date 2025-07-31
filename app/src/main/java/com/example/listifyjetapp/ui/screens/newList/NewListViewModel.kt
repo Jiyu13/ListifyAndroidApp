06230551d274c49830330c874373a1345d87b80f
jiyu13 <jiyucoding@yahoo.com>
2025-07-31 12:04:08 -0400

    delete file

--- app/src/main/java/com/example/listifyjetapp/ui/screens/newList/NewListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.listifyjetapp.ui.screens.newList
-
-import com.example.listifyjetapp.repository.ListsRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-//@HiltViewModel
-//class NewListViewModel @Inject constructor(private val repository: ListsRepository){
-//}
