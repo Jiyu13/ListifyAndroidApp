7d805695a55001ccc0e1c08e140ad99c08649288
jiyu13 <jiyucoding@yahoo.com>
2025-07-31 21:58:43 -0400

    - implement reusable filled button

--- app/src/main/java/com/example/listifyjetapp/ui/screens/auth/ListifyLoginScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/screens/auth/ListifyLoginScreen.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.listifyjetapp.components.inputFields.PasswordTextField
 import com.example.listifyjetapp.widgets.ListifyTopBar
 import com.example.listifyjetapp.components.inputFields.ValidatingInputTextField
+import com.example.listifyjetapp.ui.navigation.ListifyScreens
+import com.example.listifyjetapp.ui.theme.ListifyColor
+import com.example.listifyjetapp.widgets.FilledButton
 
 @Composable
 fun ListifyLoginScreen(
@@ -34,7 +41,9 @@ fun ListifyLoginScreen(
         ) }
     ) { innerPadding ->
         Surface(
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
         ) {
 
             Column(
@@ -49,6 +58,17 @@ fun ListifyLoginScreen(
                 PasswordTextField(
                     password = password.value,
                     onPasswordChange = { password.value = it }
+                )
+
+                FilledButton(
+                    modifier=Modifier.fillMaxWidth().padding(16.dp),
+                    shape=RoundedCornerShape(3.dp),
+                    containerColor=ListifyColor.SplashYellow,
+                    contentColor = ListifyColor.TextDark,
+                    text="LOG IN ",
+                    buttonIcon=Icons.AutoMirrored.Filled.ExitToApp,
+                    iconDescription="Log In",
+                    onClick={ navController.navigate(ListifyScreens.LoginScreen.route) }
                 )
 
             }
