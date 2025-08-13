8f005bc3dc4847f50fe9e46bb21c871d4983d51c
jiyu13 <jiyucoding@yahoo.com>
2025-08-12 21:37:54 -0400

    - inject storageManager - add errorMessage mutable state - wrap calling repository inside try/finally statement

--- app/src/main/java/com/example/listifyjetapp/ui/screens/lists/ListsViewModel.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/screens/lists/ListsViewModel.kt
@@ -1,31 +1,43 @@
 package com.example.listifyjetapp.ui.screens.lists
 
+import android.util.Log
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import com.example.listifyjetapp.data.ListifyResult
+import com.example.listifyjetapp.data.ListifyStorageManager
 import com.example.listifyjetapp.model.ListModel
 import com.example.listifyjetapp.model.ListName
 import com.example.listifyjetapp.repository.ListsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ListsViewModel @Inject constructor(private val repository: ListsRepository): ViewModel() {
+class ListsViewModel @Inject constructor(
+    private val repository: ListsRepository,
+    private val storageManager: ListifyStorageManager
+): ViewModel() {
     val lists = mutableStateListOf<ListModel>()
-    var isLoading = mutableStateOf(false)
+    var isLoading by mutableStateOf(false)
+    var errorMessage by mutableStateOf<String?>(null)
 
     private val _navigateBack = MutableStateFlow(false)
     val navigateBack = _navigateBack.asStateFlow()
 
-    fun getUserLists(userId: Int) {
+    fun getUserLists() {
         viewModelScope.launch {
-                isLoading.value = true
+            val userId = storageManager.getUser().first().userId
+            isLoading = true
+            errorMessage = null
+
+            try {
                 val result = repository.getUserLists(userId = userId)
                 when (result) {
                     is ListifyResult.Success -> {
@@ -33,15 +45,20 @@ class ListsViewModel @Inject constructor(private val repository: ListsRepository
                         lists.addAll(result.data)
                     }
 
-                    is ListifyResult.Failure -> Unit
+                    is ListifyResult.Failure -> {
+                        errorMessage = result.errorMessage
+                        Log.d("Fail to fetch lists by user id", result.toString())
+                    }
                 }
-                isLoading.value = false
+            } finally {
+                isLoading = false
+            }
         }
     }
 
     fun insertListByUser(userId: Int, newListName: ListName)
     = viewModelScope.launch {
-        isLoading.value = true
+        isLoading = true
         val result = repository.insertListByUser(userId, newListName)
         when (result) {
             is ListifyResult.Success -> {
@@ -49,7 +66,7 @@ class ListsViewModel @Inject constructor(private val repository: ListsRepository
             }
             is ListifyResult.Failure -> Unit
         }
-        isLoading.value = false
+        isLoading = false
     }
 
     fun navigationComplete() {
