1b9a7082c9acfe404c4bfb06ca6d5d0b93f626d8
jiyu13 <jiyucoding@yahoo.com>
2025-08-14 11:18:09 -0400

    Create screen - show item description

---
+++ app/src/main/java/com/example/listifyjetapp/ui/screens/listItem/ListifyListItemScreen.kt
@@ -0,0 +1,111 @@
+package com.example.listifyjetapp.ui.screens.listItem
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.listifyjetapp.R
+import com.example.listifyjetapp.utils.filterListItems
+import com.example.listifyjetapp.widgets.ListifySearchBar
+import com.example.listifyjetapp.widgets.ListifyTopBar
+
+@Composable
+fun ListifyListItemScreen(
+    navController: NavController,
+    listId: Int,
+    listName: String,
+    viewModel: ListItemViewModel = hiltViewModel()
+) {
+    LaunchedEffect(Unit) { viewModel.getAllItems(listId) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { ListifyTopBar(
+            title = listName.replace("-", " "),
+            isListsScreen = false,
+            goBackIcon = Icons.AutoMirrored.Filled.ArrowBack,
+            onGoBackButtonClicked = {navController.popBackStack()},
+            rightIcon = Icons.Default.Add,
+            onRightButtonClick = {
+                // TODO: add new item
+            }
+        ) }
+    ) { innerPadding ->
+
+        Surface(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                val searchTextState = remember { mutableStateOf("") }
+                val keyboardController = LocalSoftwareKeyboardController.current
+
+                ListifySearchBar(
+                    searchTextValue = searchTextState,
+                    onValueChange = {searchTextState.value = it},
+                    keyboardAction = KeyboardActions{
+                        searchTextState.value.trim()            // perform the search
+                        keyboardController?.hide()              // hide keyboard
+                    }
+                )
+
+                viewModel.errorMessage?.let { msg ->
+                    Text(msg,
+                        Modifier.padding(16.dp),
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+
+                if (viewModel.isLoading) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                } else if (viewModel.listItems.isEmpty()) {
+                    Text(text = stringResource(R.string.no_lists))
+                } else {
+                    LazyColumn(modifier = Modifier.padding(
+                        vertical = 16.dp,
+                        horizontal = 8.dp
+                    )){
+                        val results = filterListItems(searchTextState.value, viewModel.listItems)
+                        items(results) {item ->
+                            // TODO: ItemRow
+                            Text(item.description)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
