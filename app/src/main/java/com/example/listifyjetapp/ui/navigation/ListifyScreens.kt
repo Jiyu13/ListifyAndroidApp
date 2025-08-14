3d7064dc23cc059919495d76fe35d686fdc9caeb
jiyu13 <jiyucoding@yahoo.com>
2025-08-13 21:39:27 -0400

    - define type-safe route with @Serializable - pass in args

--- app/src/main/java/com/example/listifyjetapp/ui/navigation/ListifyScreens.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/navigation/ListifyScreens.kt
@@ -6,7 +6,9 @@ import kotlinx.serialization.Serializable
 object ListifyScreens {
     @Serializable object SplashScreen
     @Serializable data class ListsScreen(val userId: Int)
-    @Serializable object DetailScreen
+    @Serializable data class ListItemScreen(
+        val listId: Int, val listName: String
+    )
     @Serializable object ProfileScreen
     @Serializable object LoginScreen
     @Serializable object SignupScreen
