ae9f464c7bf7ebbe19bd35472810815b4692e42a
jiyu13 <jiyucoding@yahoo.com>
2025-08-13 22:14:20 -0400

    - create repository - getItemsByListId to fetch items by listId from api

---
+++ app/src/main/java/com/example/listifyjetapp/repository/ListItemRepository.kt
@@ -0,0 +1,20 @@
+package com.example.listifyjetapp.repository
+
+import com.example.listifyjetapp.data.ListifyResult
+import com.example.listifyjetapp.model.ListItem
+import com.example.listifyjetapp.network.ListifyAPI
+import javax.inject.Inject
+
+class ListItemRepository @Inject constructor(
+    private val api: ListifyAPI
+){
+    suspend fun getItemsByListId(listId: Int): ListifyResult<List<ListItem>> {
+        try {
+            val response = api.getListItems(listId)
+            return ListifyResult.Success(data = response)
+        } catch (e: Exception) {
+            return ListifyResult.Failure(e.message ?: "Error fetching items by list id")
+        }
+    }
+
+}
