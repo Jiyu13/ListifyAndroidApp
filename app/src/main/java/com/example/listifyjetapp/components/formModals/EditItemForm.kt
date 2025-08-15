e34a866019b8df82e5e17f78ce80cb78a8acf181
jiyu13 <jiyucoding@yahoo.com>
2025-08-15 18:15:30 -0400

    implement custom outlined button

--- app/src/main/java/com/example/listifyjetapp/components/formModals/EditItemForm.kt
+++ app/src/main/java/com/example/listifyjetapp/components/formModals/EditItemForm.kt
@@ -1,16 +1,20 @@
 package com.example.listifyjetapp.components.formModals
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.listifyjetapp.ui.theme.ListifyColor
+import com.example.listifyjetapp.widgets.CustomOutlinedButton
 import com.example.listifyjetapp.widgets.FilledButton
 import com.example.listifyjetapp.widgets.FormInputField
 
@@ -45,6 +49,14 @@ fun EditItemForm(
             buttonIcon = null,
             iconDescription = null,
             onClick = { onEditFormSubmit() }
+        )
+        CustomOutlinedButton(
+            modifier = Modifier.fillMaxWidth().padding(vertical = 6.dp),
+            shape = RoundedCornerShape(10.dp),
+            border = BorderStroke(2.dp, color = ListifyColor.TextDark),
+            text = "Delete",
+            textColor = Color.Red,
+            onClick = {  }
         )
     }
 }
