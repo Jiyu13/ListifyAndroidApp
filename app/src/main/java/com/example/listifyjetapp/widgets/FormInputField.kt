71cc1569eee3f2eba6b5b4730b465d7d65f458c7
jiyu13 <jiyucoding@yahoo.com>
2025-08-18 18:32:32 -0400

    apply custom text style

--- app/src/main/java/com/example/listifyjetapp/widgets/FormInputField.kt
+++ app/src/main/java/com/example/listifyjetapp/widgets/FormInputField.kt
@@ -1,10 +1,9 @@
 package com.example.listifyjetapp.widgets
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -13,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.listifyjetapp.widgets.texts.InputLabelText
 import com.example.listifyjetapp.ui.theme.ListifyColor
 
 @Composable
@@ -23,7 +23,7 @@ fun FormInputField(
 
     TextField(
         modifier = Modifier.fillMaxWidth(),
-
+        textStyle = MaterialTheme.typography.bodyMedium,
         shape = RoundedCornerShape(12.dp),
         colors = TextFieldDefaults.colors(
             unfocusedIndicatorColor = Color.White,
@@ -40,7 +40,7 @@ fun FormInputField(
 
         value = textState.value,
         onValueChange = onValueChange,
-        placeholder = { Text(text="e.g., grocery list") },
+        placeholder = { InputLabelText(text="e.g., grocery list") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),  // Sets the keyboard to normal text input.
     )
 }
