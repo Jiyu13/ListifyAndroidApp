3df7ee7bd78d7e92fcf3a8d6de97fea06a06abbb
jiyu13 <jiyucoding@yahoo.com>
2025-08-18 22:34:32 -0400

    - organize code

--- app/src/main/java/com/example/listifyjetapp/ui/navigation/ListifyScreens.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/navigation/ListifyScreens.kt
@@ -6,12 +6,10 @@ import kotlinx.serialization.Serializable
 object ListifyScreens {
     @Serializable object SplashScreen
     @Serializable data class ListsScreen(val userId: Int)
-    @Serializable data class ListItemScreen(
-        val listId: Int, val listName: String
-    )
+    @Serializable object NewListScreen
+    @Serializable data class ListItemScreen(val listId: Int, val listName: String)
     @Serializable object AddNewItemScreen
     @Serializable object ProfileScreen
     @Serializable object LoginScreen
     @Serializable object SignupScreen
-    @Serializable object NewListScreen
 }
