d37618b695d84fd86d78a0bdb04c0e5cb555276b
jiyu13 <jiyucoding@yahoo.com>
2025-08-23 01:05:26 -0400

    - add padding

--- app/src/main/java/com/example/listifyjetapp/widgets/texts/InputLabelText.kt
+++ app/src/main/java/com/example/listifyjetapp/widgets/texts/InputLabelText.kt
@@ -1,13 +1,15 @@
 package com.example.listifyjetapp.widgets.texts
 
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun InputLabelText(text: String) {
     Text(
         text = text,
-        style = MaterialTheme.typography.labelLarge
+        modifier = Modifier.padding(bottom = 8.dp)
     )
 }
