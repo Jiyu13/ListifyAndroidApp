b9d053d84bf5e25fd5f1e96d115ea1e319739c99
jiyu13 <jiyucoding@yahoo.com>
2025-08-28 21:24:55 -0400

    - add selectedIcon field

--- app/src/main/java/com/example/listifyjetapp/data/NavBarItems.kt
+++ app/src/main/java/com/example/listifyjetapp/data/NavBarItems.kt
@@ -1,24 +1,29 @@
 package com.example.listifyjetapp.data
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.FormatListBulleted
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PersonOutline
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class NavBarItems(
     val route: String,
     val title: String,
-    val icon: ImageVector
+    val icon: ImageVector,
+    val selectedIcon: ImageVector
 ) {
     Lists(
         route = "lists",
         title = "Lists",
-        icon = Icons.AutoMirrored.Filled.List
+        icon = Icons.AutoMirrored.Filled.FormatListBulleted,
+        selectedIcon = Icons.AutoMirrored.Filled.FormatListBulleted,
     ),
 
     Profile(
         route = "profile",
-        title = "Profile",
-        icon = Icons.Default.Person
+        title = "You",
+        icon = Icons.Default.PersonOutline,
+        selectedIcon = Icons.Default.Person,
     )
 }
