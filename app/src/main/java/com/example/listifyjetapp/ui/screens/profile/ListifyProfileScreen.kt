a2b5d2bde2e92915f6d3b9bf4615c70682ffeafa
jiyu13 <jiyucoding@yahoo.com>
2025-09-09 19:48:28 -0400

    - remove top bar right save button - add filled button

--- app/src/main/java/com/example/listifyjetapp/ui/screens/profile/ListifyProfileScreen.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/screens/profile/ListifyProfileScreen.kt
@@ -1,5 +1,6 @@
 package com.example.listifyjetapp.ui.screens.profile
 
+import android.R.attr.thickness
 import android.view.Gravity
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.CircularProgressIndicator
@@ -34,8 +37,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.listifyjetapp.data.LoginState
+import com.example.listifyjetapp.ui.theme.ButtonPaddings
 import com.example.listifyjetapp.ui.theme.ListifyColor
 import com.example.listifyjetapp.widgets.bars.ListifyTopBar
+import com.example.listifyjetapp.widgets.buttons.FilledButton
 import com.example.listifyjetapp.widgets.buttons.TextButton
 import com.example.listifyjetapp.widgets.dialogs.AlertDialogPopup
 import com.example.listifyjetapp.widgets.dividers.InputDivider
@@ -101,8 +106,6 @@ fun ListifyProfileScreen(
         topBar = { ListifyTopBar(
             title = "Profile",
             isListsScreen = false,
-            rightText = "Save",
-            onRightButtonClick = { onSaveUsername() }
         ) }
     ) { innerPadding ->
 
@@ -143,6 +146,19 @@ fun ListifyProfileScreen(
                         )
                     }
 
+                    Column(modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp)) {
+                        FilledButton(
+                            modifier = ButtonPaddings.fillMaxWidth(),
+                            shape = RoundedCornerShape(3.dp),
+                            containerColor=ListifyColor.SplashYellow,
+                            contentColor = ListifyColor.TextDark,
+                            text="Save",
+                            onClick={ onSaveUsername() }
+                        )
+                    }
+
+                    HorizontalDivider(thickness = 1.dp)
+
                     ResetPasswordButton(goToReSetPW = goToReSetPW)
 
                     HorizontalDivider(thickness = 1.dp)
@@ -170,18 +186,6 @@ fun ListifyProfileScreen(
                     //    onValueChange = { emailInput = it }
                     //)
 
-                    //Spacer(modifier = Modifier.size(16.dp))
-
-                    //FilledButton(
-                    //    modifier = ButtonPaddings.fillMaxWidth(),
-                    //    shape = ButtonShape,
-                    //    containerColor = ListifyColor.SplashYellow,
-                    //    contentColor = Color.White,
-                    //   text = "Save",
-                    //    buttonIcon = null,
-                    //    iconDescription = null,
-                    //    onClick = {  }
-                    //)
                 }
             }
 
