49d74c0f0116168998964e1aa8cdc5c6c3baf3d8
jiyu13 <jiyucoding@yahoo.com>
2025-09-25 19:35:36 -0400

    edit font sizes

--- app/src/main/java/com/example/listifyjetapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/listifyjetapp/ui/theme/Type.kt
@@ -12,7 +12,7 @@ val Typography = Typography(
         // Main content
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 24.sp,
+        fontSize = 20.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
@@ -21,7 +21,7 @@ val Typography = Typography(
         // input fields, topBar text buttons
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 20.sp,
+        fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
@@ -30,7 +30,7 @@ val Typography = Typography(
         // login error
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 14.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
@@ -39,7 +39,7 @@ val Typography = Typography(
         // topBar tittles
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.ExtraBold,
-        fontSize = 28.sp,
+        fontSize = 24.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
@@ -56,7 +56,7 @@ val Typography = Typography(
         // searchBar text, divider w/ text, profile email
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
+        fontSize = 16.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
